a46a8599a27d95c2372da63de34f3bd4dffcc529
Jim van Eijk <info@jimvaneijk.com>
2020-11-16 15:51:42 +0100

    Default option is label at giftwrapping

--- templates/woo-additional-gift-wrapping/gift-wrapping-option.php
+++ templates/woo-additional-gift-wrapping/gift-wrapping-option.php
@@ -3,19 +3,16 @@
 >
   <?php if($type === 'select'): ?>
     <div class="additional-gift-wrapping__select">
-      <label for="gift_wrap">
-          <?php echo str_replace(['{price}'], [$price], wp_kses_post($label)); ?>
-      </label>
       <select
           name="gift_wrap"
           id="gift_wrap"
           class="select"
       >
         <option value="">
-            <?php echo __('No', 'woocommerce-product-gift-wrap') ?>
+            <?php echo __('Gift wrapping ?', 'woocommerce-product-gift-wrap') ?>
         </option>
         <option value="Yes">
-            <?php echo __('Yes', 'woocommerce-product-gift-wrap') ?> <?php if($price) { echo '(+'. $price . ')'; }?>
+            <?php echo __('Yes, add gift wrapping', 'woocommerce-product-gift-wrap') ?> <?php if($price) { echo '(+'. $price . ')'; }?>
         </option>
       </select>
     </div>
