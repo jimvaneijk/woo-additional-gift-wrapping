c646181a9788e6d8a925a5cc0249c2ab92574d27
Jim van Eijk <info@jimvaneijk.com>
2020-11-17 10:13:35 +0100

    Change way of working

--- templates/woo-additional-gift-wrapping/gift-wrapping-option.php
+++ templates/woo-additional-gift-wrapping/gift-wrapping-option.php
@@ -1,27 +1,24 @@
 <div
     class="additional-gift-wrapping"
 >
-  <?php if($type === 'select'): ?>
-    <div class="additional-gift-wrapping__select">
-      <label for="gift_wrap">
-          <?php echo str_replace(['{price}'], [$price], wp_kses_post($label)); ?>
-      </label>
-      <select
-          name="gift_wrap"
-          id="gift_wrap"
-          class="select"
-      >
-        <option value="">
-            <?php echo __('No', 'woocommerce-product-gift-wrap') ?>
-        </option>
-        <option value="Yes">
-            <?php echo __('Yes', 'woocommerce-product-gift-wrap') ?> <?php if($price) { echo '(+'. $price . ')'; }?>
-        </option>
-      </select>
-    </div>
-  <?php else:?>
-    <label>
-        <?php echo str_replace(['{checkbox}', '{price}'], [$checkbox, $price], wp_kses_post($message)); ?>
-    </label>
-  <?php endif; ?>
+    <?php if($type === 'select'): ?>
+        <div class="additional-gift-wrapping__select">
+            <select
+                name="gift_wrap"
+                id="gift_wrap"
+                class="select"
+            >
+                <option value="">
+                    <?php echo __('Gift wrapping ?', 'woocommerce-product-gift-wrap') ?>
+                </option>
+                <option value="Yes">
+                    <?php echo __('Yes, add gift wrapping', 'woocommerce-product-gift-wrap') ?> <?php if($price) { echo '(+'. $price . ')'; }?>
+                </option>
+            </select>
+        </div>
+    <?php else:?>
+        <label>
+            <?php echo str_replace(['{checkbox}', '{price}'], [$checkbox, $price], wp_kses_post($message)); ?>
+        </label>
+    <?php endif; ?>
 </div>
